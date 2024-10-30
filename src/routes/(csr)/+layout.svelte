c947b09e71115c01c8b72be63b21e981d8d50619
doongjohn <doongjohn@gmail.com>
2024-10-30 23:54:53 +0900

    update

--- src/routes/(csr)/+layout.svelte
+++ src/routes/(csr)/+layout.svelte
@@ -1,12 +1,14 @@
 <script>
+  import { base } from '$app/paths'
+
 	let { children } = $props()
 </script>
 
 <nav>
 	<ul>
-		<li><a data-sveltekit-reload href="/">home</a></li>
-		<li><a href="/spa/counter-edit">counter edit</a></li>
-		<li><a href="/spa/counter-view">counter view</a></li>
+		<li><a data-sveltekit-reload href="{base}/">home</a></li>
+		<li><a href="{base}/spa/counter-edit">counter edit</a></li>
+		<li><a href="{base}/spa/counter-view">counter view</a></li>
 	</ul>
 </nav>
 
--- src/routes/(ssg)/+layout.svelte
+++ src/routes/(ssg)/+layout.svelte
@@ -1,13 +1,15 @@
 <script>
+  import { base } from '$app/paths'
+
 	let { children } = $props()
 </script>
 
 <nav>
 	<ul>
-		<li><a data-sveltekit-reload href="/">home</a></li>
-		<li><a data-sveltekit-reload href="/about">about</a></li>
-		<li><a data-sveltekit-reload href="/hello">hello</a></li>
-		<li><a data-sveltekit-reload href="/spa">spa</a></li>
+		<li><a data-sveltekit-reload href="{base}/">home</a></li>
+		<li><a data-sveltekit-reload href="{base}/about">about</a></li>
+		<li><a data-sveltekit-reload href="{base}/hello">hello</a></li>
+		<li><a data-sveltekit-reload href="{base}/spa">spa</a></li>
 	</ul>
 </nav>
 {@render children()}
